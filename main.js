8e13184da42097ec869b2037bce8809ce95952f9
Cesar Casco <eduardomelchor34@gmail.com>
2023-08-07 13:21:57 -0600

    funciones-generadoras-async

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
 import { forAwaitComponent } from './src/concepts/08-for-await';
 import { generatorFunctionsComponent } from './src/concepts/09-generators';
+import { generatorAsyncComponent } from './src/concepts/10-generator-async';
 
 
 document.querySelector('#app').innerHTML = `
@@ -41,7 +42,8 @@ const element = document.querySelector('.card');
 // asyncAwaitComponent(element);
 // asyncAwait2Component(element);
 // forAwaitComponent(element);
-generatorFunctionsComponent(element);
+// generatorFunctionsComponent(element);
+generatorAsyncComponent(element);
 
 
 
