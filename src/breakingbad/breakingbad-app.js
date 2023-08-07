3e1985c6fa7a13bf627658a379a50fff378b3fbb
Cesar Casco <eduardomelchor34@gmail.com>
2023-08-07 17:42:12 -0600

    http-app

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,14 @@
+
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const Breakingbadapp = ( element ) => {
+    document.querySelector('#app-tittle').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+    
+}
+
