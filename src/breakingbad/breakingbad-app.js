64a4a7d539196b9b2cdd81ab71eb0957633b5540
Cesar Casco <eduardomelchor34@gmail.com>
2023-08-07 19:32:11 -0600

    mostrar-quote

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,4 +1,16 @@
 
+/**
+ * @returns {Promise<Object>} Quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch( 'https://animechan.xyz/api/random' );
+    const data= await res.json();
+
+    console.log(data);
+    return data;
+
+}
 
 
 /**
@@ -6,10 +18,28 @@
  * @param {HTMLDivElement} element 
  */
 
-export const Breakingbadapp = ( element ) => {
+export const Breakingbadapp = async( element ) => {
     document.querySelector('#app-tittle').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
-    
-}
+    // await fetchQuote();
 
-fbdf
+    const quoteLabel = document.createElement('blockquote');
+    const characterLabel = document.createElement('h3');
+    const animeLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderquote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        characterLabel.innerHTML = data.character;
+        animeLabel.innerHTML = data.anime;
+        element.replaceChildren( quoteLabel, characterLabel, animeLabel, nextQuoteButton );
+
+    }
+
+
+    fetchQuote()
+        .then( renderquote );
+
+
+}
